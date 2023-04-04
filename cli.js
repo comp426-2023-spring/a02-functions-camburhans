0fdca27d25b41a39cb04c29aee72ba58e929c6c8
Cam Burhans <cburhans@email.unc.edu>
2023-04-04 16:27:14 -0400

    Create logic for running on the command line

--- cli.js
+++ cli.js
@@ -18,3 +18,37 @@ if (args.h) {
 }
 
 const timezone = moment.tz.guess()
+
+
+let latitude;
+let longitude;
+let timezone_url;
+let day;
+
+if (args.n) {
+	latitude = args.n;
+} else if (args.s) {
+	latitude = -args.s
+} else if (!latitude) {
+	console.log("Latitude must be in range");
+	process.exit(0);
+}
+
+if (args.e) {
+        longitude = args.e;
+} else if (args.w) {
+        longitude = -args.w
+} else if (!longitude) {
+	console.log("Longitude must be in range");
+	process.exit(0);
+}
+
+if (args.z) {
+	timezone_url = args.z;
+} else {
+	timezone_url = timezone;
+}
+
+if (args.d) {
+	day = args.d;
+}
