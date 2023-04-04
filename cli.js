1bafaef0ddb8f1ea92f82b04c9aaed779f5f64ab
Cam Burhans <cburhans@email.unc.edu>
2023-04-04 16:29:20 -0400

    Create data variable that pulls the data from the response variable

--- cli.js
+++ cli.js
@@ -55,3 +55,5 @@ if (args.d) {
 }
 
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&timezone=' + timezone_url);
+
+const data = await response.json();
